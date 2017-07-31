a7d3c7559475b453fc8b3eba07cb49130c933558
Vani Thilagar <vthilagar@parallelwireless.com>
2017-07-31 17:28:50 -0400

    Sample firebase app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
+var firebaseConfig = {
+    apiKey: "AIzaSyDQF48Sdd3PSOwKMQFiGY1e3OroKuwY5rY",
+    authDomain: "testproject-89638.firebaseapp.com",
+    databaseURL: "https://testproject-89638.firebaseio.com",
+    projectId: "testproject-89638",
+    storageBucket: "testproject-89638.appspot.com",
+    messagingSenderId: "479143212732"
+  };
 
 @NgModule({
   declarations: [
@@ -14,7 +26,11 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +40,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { AngularFireModule } from 'angularfire2';
+import {AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  fbName: FirebaseObjectObservable<any>;
+  todos: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
+    this.fbName = this.db.object('/myname');
+    this.todos = this.db.list('/todos');
 
+  }
+  setName(newName) {
+    this.fbName.set({name:newName});
+  }
+
+  addTodo(newTodo){
+    this.todos.push({text: newTodo});
+  }
+
+  removeTodo(key){
+    this.todos.remove(key);
   }
 
 }
